9c19ef9a1d7374fbe47780f16dc4e5ba29967e00
prayer-on <prayer.onaiwu@gmail.com>
2026-02-07 22:00:42 +0100

    POST route for the login added

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -22,5 +22,32 @@ bcrypt.hash(req.body.password, 10) .then (
 };
 
 exports.login = (req, res, next) => {
-    
+ User.findOne({email: req.body.email}).then((user) => {
+    if (!user) {
+        return res.status(401).json({
+            error: new Error ('User not found!')
+        });
+    }
+    bcrypt.compare(req.body.password, user.password) .then((valid) => {
+        if (!valid) {
+            return res.status(401).json({
+                error: new Error ('Incorrect password!')
+            });
+        }
+    res.status(200),json({
+        userId: user._id,
+        token: 'token'
+    })
+    })
+    .catch((error) => {
+        res.status(500).json({
+            error: error
+        })
+    })
+ })
+ .catch((error) => {
+    res.status(500).json({
+        error: error
+    })
+ })
 }
